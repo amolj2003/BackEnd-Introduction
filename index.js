e8e96d98b41208e701e34dc14738d3fbbd5eb997
AMOL JAISWAL <amoljaiswal2231@gmail.com>
2024-06-04 09:04:51 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 require("dotenv").config();
+const cors=require('cors')
 
 const app = express();
+app.use(cors());
 const portfolioRoute =require("./routes/portfolioRoutes");
 
 app.use("/api/portfolio",portfolioRoute);
